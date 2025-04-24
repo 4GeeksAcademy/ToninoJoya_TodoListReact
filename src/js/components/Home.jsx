8102f9bbbbd63e6f1a966fc3f53114257eef687d
ToninoJoya <antoniocaprab@gmail.com>
2025-04-24 21:43:33 +0000

    ensayo todolist

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,88 @@
 import React from "react";
+import { useState } from "react";
+import { Lista } from "./Lista";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+	<Lista/>
+	// const [data, setData] = useState({
+	// 	name: "",
+	// 	email:"",
+	// 	task:""
+	// })
+
+	// const [Listdata, SetListData] = useState([])
+
+
+	// const handleChange = (event) => {
+
+		// console.log(`{${event.target.name}: ${event.target.value}}`)
+	// 		setData({
+	// 			...data,
+	// 			[event.target.name]:event.target.value
+	// 		})
+	// }
+
+
+
+
+	// const addTask = (event) => {
+		
+	// 	if(event.key=="Enter"){
+			
+	// 		SetListData([
+	// 			...Listdata,
+	// 			data
+	// 		])
+	// 		setData({
+	// 			task:"",
+	// 			name:"",
+	// 			email:""
+				
+	// 		})
+
+	// 	}
+	// }
+
+	// return (
+
+	// 	<div className="container">
+	// 		<div className="row justify-content-center">
+	// 			<div className="col-12 col-md-7 border border-danger">
+	// 				<h1 className="text-center">To do List</h1>
+	// 				<form className="border p-3 mb-2">
+	// 					<input type="text"
+	// 						className="form-control mt-2"
+	// 						placeholder="Ingresa tu tarea"
+	// 						name="task"
+	// 						value={data.task}
+	// 						onKeyDown={addTask}
+	// 						onChange={handleChange}
+	// 					/>
+	// 					<input type="text"
+	// 						className="form-control mt-2"
+	// 						placeholder="Ingresa tu nombre"
+	// 						name="name"
+	// 						value={data.name}
+	// 						onKeyDown={addTask}
+	// 						onChange={handleChange} 
+	// 					/>
+	// 					<input type="text"
+	// 						className="form-control mt-2"
+	// 						placeholder="Ingresa tu correo"
+	// 						name="email"
+	// 						value={data.email}
+	// 						onKeyDown={addTask}
+	// 						onChange={handleChange}
+	// 					/>
+
+
+	// 				</form>
+	// 			</div>
+	// 		</div>
+	// 	</div>
 };
 
 export default Home;
